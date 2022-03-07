69d9abe843a08ccb1d1affeb2ffa5a865fcedc82
wmaf <wmaf@outlook.com>
2022-03-07 13:02:35 -0500

    Initial commit

---
+++ src/main/java/com/careerdevs/hellointernet/HelloInternetApplication.java
@@ -0,0 +1,13 @@
+package com.careerdevs.hellointernet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloInternetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloInternetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/careerdevs/hellointernet/HelloInternetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerdevs.hellointernet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloInternetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
