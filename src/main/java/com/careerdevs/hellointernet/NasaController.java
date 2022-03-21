aca61a729dc8ec49f45df15408c061e0aa82412a
wmaf <wmaf@outlook.com>
2022-03-21 18:49:06 -0400

    Nasa Home Work

--- src/main/java/com/careerdevs/hellointernet/NasaController.java
+++ src/main/java/com/careerdevs/hellointernet/NasaController.java
@@ -8,15 +8,19 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class NasaController {
-
 //    @GetMapping("/nasa")
 //    public Object nasaPicOfDay(RestTemplate restTemplate) {
 //        Object requestData = restTemplate.getForObject("https://api.nasa.gov/planetary/apod?api_key=EyYqFD5EeIjFv1oDtWej5fAYlywEOqJwIqr1r2yz", Object.class);
 //        return requestData;
-// }
-    @GetMapping("/pickFive")
-    public Object pickFive(RestTemplate restTemplate) {
-        Object requestFive = restTemplate.getForObject("https://api.nasa.gov/planetary/apod?api_key=EyYqFD5EeIjFv1oDtWej5fAYlywEOqJwIqr1r2yz", Object.class);
-        return requestFive;
+//  }
+//    @GetMapping("/pickFive")
+//    public Object pickFive(RestTemplate restTemplate) {
+//        Object requestFive = restTemplate.getForObject("https://api.nasa.gov/planetary/apod?api_key=EyYqFD5EeIjFv1oDtWej5fAYlywEOqJwIqr1r2yz", Object.class);
+//        return requestFive;
+//  }
+    @GetMapping ("/getPhotosInRange")
+    public Object getPhotosInRange(RestTemplate restTemplate) {
+        Object getDateRange = restTemplate.getForObject("https://api.nasa.gov/planetary/apod?start_date=2015-09-07&end_date=2015-09-08&api_key=EyYqFD5EeIjFv1oDtWej5fAYlywEOqJwIqr1r2yz",Object.class);
+        return getDateRange;
     }
 }
