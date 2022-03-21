8fe6ad0fc932ce454e23ad6e9d0d05d75fd0edf1
wmaf <wmaf@outlook.com>
2022-03-21 18:19:02 -0400

    Nasa Home Work

--- src/main/java/com/careerdevs/hellointernet/NasaController.java
+++ src/main/java/com/careerdevs/hellointernet/NasaController.java
@@ -5,12 +5,18 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 
+
 @RestController
 public class NasaController {
 
-    @GetMapping("/nasa")
-    public Object nasaPicOfDay(RestTemplate restTemplate) {
-        Object requestData = restTemplate.getForObject("https://api.nasa.gov/planetary/apod?api_key=EyYqFD5EeIjFv1oDtWej5fAYlywEOqJwIqr1r2yz", Object.class);
-        return requestData;
+//    @GetMapping("/nasa")
+//    public Object nasaPicOfDay(RestTemplate restTemplate) {
+//        Object requestData = restTemplate.getForObject("https://api.nasa.gov/planetary/apod?api_key=EyYqFD5EeIjFv1oDtWej5fAYlywEOqJwIqr1r2yz", Object.class);
+//        return requestData;
+// }
+    @GetMapping("/pickFive")
+    public Object pickFive(RestTemplate restTemplate) {
+        Object requestFive = restTemplate.getForObject("https://api.nasa.gov/planetary/apod?api_key=EyYqFD5EeIjFv1oDtWej5fAYlywEOqJwIqr1r2yz", Object.class);
+        return requestFive;
     }
 }
